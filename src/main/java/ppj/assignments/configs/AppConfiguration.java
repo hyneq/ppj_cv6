4b6be09933f30c3a0c9318b6a454362e5573aedc
hyneq <hynekv.svobodny@gmail.com>
2025-03-15 14:50:50 +0100

    Updated spring-boot-starter-parent to version 3.4.3

--- src/main/java/ppj/assignments/configs/AppConfiguration.java
+++ src/main/java/ppj/assignments/configs/AppConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 import java.util.Arrays;
 
 @Component
